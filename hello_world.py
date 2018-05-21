67aa5cc33d6d1afeb322db6194a4644fefec4981
tkoar <terrancekoar@gmail.com>
2018-05-20 22:37:56 -0400

    solution

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,25 @@
 # import flask here
+from flask import Flask
 
 # create new Flask App here
+app = Flask(__name__)
 
 # define routes for your new flask app
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/home')
+def index():
+    return "Welcome to an amazing Flask App!"
+
+@app.route('/myprofile')
+def index():
+    return "This is my profile! It's not finished yet... :/"
+
+@app.route('/exit')
+def index():
+    return  "Thanks for looking around. Come back again soon!"
 
 # tell your flask app to run with debug mode on
+app.run(debug=True)
